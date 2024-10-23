7490d48dfc1c67b02ccae8f3498e496a809efacd
Minemario64 <160744660+Minemario64@users.noreply.github.com>
2024-10-22 22:16:38 -0400

    developing

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from getfile import *
 
 root = Tk()
 
-is_db_empty("translates.db")
+isDbEmpty= is_db_empty("translates.db")
 
 connection = sqlite3.connect("translates.db")
 
 cursor = connection.cursor()
-cursor.execute("""CREATE TABLE translates (id INTEGER PRIMARY KEY, Spanish TEXT, English TEXT, Class_id INTEGER)
+if isDbEmpty:
+    cursor.execute("""CREATE TABLE translates (id INTEGER PRIMARY KEY, Spanish TEXT, English TEXT, Class_id INTEGER)
             """)
 
 root.mainloop()
