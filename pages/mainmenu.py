a659900748e20373f0dc54d29146bb69b82f1303
Minemario64 <160744660+Minemario64@users.noreply.github.com>
2024-10-23 23:04:06 -0400

    Day 2 developing

--- pages/mainmenu.py
+++ pages/mainmenu.py
@@ -9,12 +9,12 @@ def cards_select(root, type):
     menu_var = StringVar(value="Select a Flashcard Pack")
     menu_options = ["PS - Element Orders", "ESP - Direct Object Pronouns"]
 
+    def export_menu(menu_var):
+        print(menu_var)
+
     label = ttk.Label(root, text="Select Flashcard Pack", style='TLabel').pack(pady=10)
     spacer_1 = ttk.Label(root, text="|", foreground="#191919").pack(pady=30)
-    menu = OptionMenu(root, menu_var, *menu_options).pack(pady=10)
+    menu = OptionMenu(root, menu_var, *menu_options, command=export_menu).pack(pady=10)
     spacer_2 = ttk.Label(root, text="|", foreground="#191919").pack(pady=10)
 
-    def export_menu(menu):
-        print(menu.get)
-
-    start_button = ttk.Button(root, text="Start A Training Session", command=lambda: export_menu(menu)).pack()
+    start_button = ttk.Button(root, text="Start A Training Session", command=lambda: export_menu(menu_var)).pack()
