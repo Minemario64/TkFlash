6396b9012bc4d00a53b67c011b1b2afd97eb6505
Minemario64 <160744660+Minemario64@users.noreply.github.com>
2024-10-23 20:34:35 -0400

    Day 2 developing

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from tkinter import *
 from PIL import ImageTk,Image
 import sqlite3
 from getfile import *
+from pages import *
 
 root = Tk()
+root.config(background="#191919")
+root.geometry("500x500")
 
 def change_pages(cur_page,new_page):
     if cur_page == new_page:
         return "Error : Same Page"
-    closing_page = getattr(PAGES, cur_page)
+    closing_page = getattr(pages, cur_page) # type: ignore
     closed = closing_page(root, "clear")
 
 isDbEmpty= is_db_empty("translates.db")
@@ -20,6 +23,7 @@ if isDbEmpty:
     cursor.execute("""CREATE TABLE translates (id INTEGER PRIMARY KEY, Spanish TEXT, English TEXT, Class_id INTEGER)
             """)
 
+cards_select(root, 1)
 
 connection.close()
 
--- pages.py
+++ pages.py
@@ -1,5 +1,14 @@
 from tkinter import *
 from PIL import ImageTk,Image
+from tkinter import ttk
 
 def cards_select(root, type):
-    pass
+    style = ttk.Style()
+    style.configure('TLabel', font=('Helvetica', 16, 'bold'), foreground="white", background="#191919")
+
+    menu_var = StringVar(value="Select a Flashcard Pack")
+    menu_options = ["PS - Element Orders", "ESP - Direct Object Pronouns"]
+
+    label = ttk.Label(root, text="Select Flashcard Pack", style='TLabel').pack(pady=10)
+    spacer_1 = ttk.Label(root, text="|", foreground="#191919").pack(pady=50)
+    menu = OptionMenu(root, menu_var, *menu_options).pack(pady=10)
