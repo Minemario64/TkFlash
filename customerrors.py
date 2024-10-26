347cd578460bb3a25e4d13ae1a26304ced767708
Minemario64 <160744660+Minemario64@users.noreply.github.com>
2024-10-26 19:06:54 -0400

    Day 5 developing
    - Added querying function
    - Added a custom error system
    - Added data to db

---
+++ customerrors.py
@@ -0,0 +1,14 @@
+from colorama import init, Fore, Back, Style
+
+init()
+
+class TkFlashError(Exception):
+    colors = [Fore.BLACK, Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN, Fore.WHITE]
+    bgs = [Back.BLACK, Back.RED, Back.GREEN, Back.YELLOW, Back.BLUE, Back.MAGENTA, Back.CYAN, Back.WHITE]
+    brightnesses = [Style.DIM, Style.NORMAL, Style.BRIGHT]
+    prefixes = {"error" : Fore.RED, "warning" : Fore.YELLOW}
+    def __init__(self, type, message):
+        prefix = self.prefixes[type]
+        ending = Style.RESET_ALL
+        self.message = f"{prefix}{message}{ending}"
+        super().__init__(self.message)
--- getfile.py
+++ getfile.py
@@ -1,8 +1,8 @@
-import sqlite3
+from sqlite3 import *
 
 def is_db_empty(db_path):
 
-    conn = sqlite3.connect(db_path)
+    conn = connect(db_path)
     cursor = conn.cursor()
 
     cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
@@ -11,3 +11,23 @@ def is_db_empty(db_path):
     if not tables:
         conn.close()
         return True
+
+def tkflash_query_db(db : str, dbTable : str, dbColumnList : list[str], dbCallList : list[int], class_id : int, allColumns : bool = True):
+
+    connection = connect(db)
+    cursor = connection.cursor()
+
+    if allColumns:
+        cursor.execute(f"SELECT * FROM {dbTable} WHERE class_id = {class_id};")
+    else:
+        i = 0
+        for column in dbCallList:
+            i += 1
+            if i == 1:
+                qcolumn = f"{dbColumnList[column]}, "
+            else:
+                qcolumn += f"{dbColumnList[column]}, "
+        qcolumn = qcolumn[:-2]
+
+        cursor.execute(f"SELECT {qcolumn} FROM {dbTable} WHERE class_id = {class_id};")
+        print(cursor.fetchall())
